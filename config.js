5e848aceebf0684556c86c463a698190773afc5d
pyotr_chan <pollyfeb@outlook.com>
2019-06-25 17:54:31 +0900

    change font family

--- config.js
+++ config.js
@@ -3,11 +3,29 @@
 module.exports = {
   url: 'https://pyotr-chan.netlify.com',
   title: 'ピョートル・ユートル',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  subtitle: '九州生まれ東京潜りのエンジニア志望。CMS初心のなかの初心者です。',
   copyright: '© Pyotr_chan All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
+  plugins: [
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Noto Sans JP`,
+            variants: [`100`, `300`],
+            subsets: [`japanese`]
+          },
+          {
+            family: `Lato`,
+            variants: [`300`]
+          },
+        ],
+      },
+    }
+  ],
   menu: [
     {
       label: 'Articles',
