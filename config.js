865e8d7c07d35a1283e886a30ba9676ead590ae5
pyotr_chan <pollyfeb@outlook.com>
2019-06-30 19:55:46 +0900

    add googleanalyticsID

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ module.exports = {
   url: 'https://pyotr-chan.netlify.com',
   title: 'ピョートル・ユートル',
   subtitle: '九州生まれ東京潜りのエンジニア志望。CMS初心のなかの初心者です。',
-  copyright: '© Pyotr_chan All rights reserved.',
+  copyright: '© pyotr_chan All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-143063197-1',
 
   plugins: [],
   menu: [
@@ -29,12 +29,12 @@ module.exports = {
     photo: '/photo.jpg',
     bio: '九州生まれ東京潜り。CMS初心のなかの初心者です。',
     contacts: {
-      email: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '#',
-      vkontakte: '#'
+      // email: '#',
+      // telegram: '#',
+      twitter: 'https://twitter.com/pyotr_chan',
+      github: 'https://github.com/pyotr-chan',
+      // rss: '#',
+      // vkontakte: '#'
     }
   }
 };
