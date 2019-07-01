67c2ac6dc453db21ac6dc9c8aa29315a1ddd6024
pyotr_chan <pollyfeb@outlook.com>
2019-07-01 18:43:02 +0900

    add iframely on  Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,6 +4,29 @@ import Helmet from 'react-helmet';
 import type { Node as ReactNode } from 'react';
 import styles from './Layout.module.scss';
 
+import React from 'react';
+import Helmet from 'react-helmet';
+
+export default class Iframely extends React.Component {
+
+  // 記事ページから古い記事に遷移したときにもロードされるようにする
+  componentDidMount () {
+    if (window.iframely) {
+      window.iframely.load();
+    }
+  }
+
+  // マークダウンファイルにembed.jsのスクリプトタグを記述しても読み込まれないため
+  // コンポーネントでReact Helmetを使って読み込む
+  render() {
+      return (
+        <Helmet>
+          <script type="text/javascript" src="https://cdn.iframe.ly/embed.js" charset="utf-8"/>
+        </Helmet>
+    );
+  }
+}
+
 type Props = {
   children: ReactNode,
   title: string,
